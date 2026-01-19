e3585bd4823ea95813189563569ce636c766924a
sonalikasamanta019-blip <sonalikasamanta019@gmail.com>
2026-01-19 17:09:19 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,23 @@
+// console.log("we are learning typescript");
+
+// function apple(){
+//     var x =10;
+//     if (x == 10){
+//         let y = 20;
+//         console.log(`x=${x} y=${y}`)
+//     }
+// }
+
+// apple();
+
+function apple(){
+    var x =10;
+    var y;
+    if (x == 10){
+        y = 20;
+    }
+    console.log(`x=${x} y=${y}`)
+}
+
+apple();
+
---
+++ datatype.ts
@@ -0,0 +1,25 @@
+//number
+/*let signed: number = -10;
+let unsigned: number = 10;
+let float: number = 5.5;
+let decimal: number = 4.32456424343;
+let exponent: number = 2e3;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+console.log(signed);
+console.log(unsigned);
+console.log(float);
+console.log(decimal);
+console.log(exponent);
+console.log(hex);
+console.log(binary);
+console.log(octal);*/
+
+//string
+let age: number = 30;
+let msg1: string = "you will be " + (age+1) + " new year";
+let msg2: string = `you will be ${age+1} new year`;
+console.log(msg1);
+console.log(msg2);
